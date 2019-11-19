a6f06cd08580455db0d399d4ea600dbb2aaab0d1
Tonpai <tonpai@Tonpais-Mac-mini.local>
2019-11-19 07:45:16 +0700

    Update unit7 no 20 excercise

---
+++ unit7_no22.py
@@ -0,0 +1,11 @@
+def Visiting():
+    count = 0
+    def counter():
+        nonlocal count
+        count = count + 1
+        return count
+    return counter
+
+counter = Visiting()
+print(counter())
+print(counter())
